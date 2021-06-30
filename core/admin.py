f82f1bc1027801ed06fada50c344a00dffc355df
SPC <s.painen@alumnos.duoc.cl>
2021-06-29 22:58:38 -0400

    v3 Definiendo MODELS

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,51 @@
 from django.contrib import admin
-from .models import Especialidad, Medico, reserva
+from .models import Especialidad, Medico, reserva, Hora, Pago
 # Register your models here.
 
-admin.site.register(Especialidad)
-admin.site.register(Medico)
+
+
 admin.site.register(reserva)
+
+#######################################
+
+class EspecialidadAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'especialidad'
+    )   
+
+admin.site.register(Especialidad, EspecialidadAdmin)
+
+#######################################
+
+class HoraAdmin(admin.ModelAdmin):
+    list_display =(
+        'id',
+        'fecha_reservaR',
+    )
+
+admin.site.register(Hora, HoraAdmin)
+
+#######################################
+
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'rut',
+        'nombreM',
+        'apaterno',
+        'especialidad'
+    )   
+
+admin.site.register(Medico, MedicoAdmin)
+
+#######################################
+
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'montoR',
+    )   
+
+
+admin.site.register(Pago, MedicoAdmin)
--- core/models.py
+++ core/models.py
@@ -5,25 +5,43 @@ from django.db.models.fields.related import ForeignKey
 class Especialidad(models.Model):
     especialidad = models.CharField(max_length=80)
 
+    def __str__(self):
+        return self.especialidad
+
 class Medico(models.Model):
     rut = models.CharField(max_length=11)
     nombreM = models.CharField(max_length=80)
     apaterno = models.CharField(max_length=80)
     especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.nombreM + ' ' +self.apaterno + ' - id especialidad: ' + str(self.especialidad)
+
+class Pago(models.Model):
+    montoR = models.FloatField()
+
+    def __str__(self):
+        return str(self.id) + ' ' + str(self.montoR)
+
+class Hora(models.Model):
+    fecha_reservaR = models.DateTimeField(null=True, blank=True)
+    nombreM = models.ManyToManyField(Medico)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + str(self.fecha_reservaR) + ' - ' +str(self.nombreM)
 
 class reserva(models.Model):
     rut = models.CharField(max_length=11)
-    nombre = models.CharField(max_length=80)
-    apaterno = models.CharField(max_length=80)
-    amaterno = models.CharField(max_length=80)
+    nombre = models.CharField(max_length=80, blank=True)
+    apellidos = models.CharField(max_length=100, blank=True)
     correo = models.EmailField(max_length = 200)
-    fecha_nacimiento = models.CharField(max_length=80)
+    fecha_nacimiento = models.DateField()
     sexo = models.CharField(max_length=20)
     especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE)
-    nombreM = models.ForeignKey(Medico, on_delete=models.CASCADE)
-    fecha_reservaR = models.CharField(max_length=80)
-    montoR = models.FloatField()
-    horaR = models.CharField(max_length=80)
+    nombreM = models.ForeignKey(Hora, on_delete=models.CASCADE, related_name = 'nombre_medico')
+    pago = models.ForeignKey(Pago, on_delete=models.CASCADE, blank=True, null=True)
+    fecha_hora = models.ForeignKey(Hora, on_delete=models.CASCADE, related_name='fecha_hora_reserva', blank=True, null=True)
 
 
 
